2167ab21bde99a1a2239ffe9a79502f232be22b4
pixelomer <37103802+pixelomer@users.noreply.github.com>
2023-03-10 21:40:53 +0300

    Don't assume compatibility mode

--- src/main.c
+++ src/main.c
@@ -70,7 +70,7 @@ int main(int argc, char **argv) {
 	bool write_to_tty = false;
 	int framerate = DEFAULT_FRAMERATE;
 	const char *encode_flags = DEFAULT_FFMPEG;
-	bool isg_mode = true;
+	bool isg_mode = false;
 
 	int opt;
 	bool opts[0x100] = {};
